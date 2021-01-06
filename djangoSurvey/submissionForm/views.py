a4260e2dd86e2b9d08154f9470546e7ea63c1ea6
achan <achan@achans-MacBook-Pro.local>
2021-01-06 14:24:16 -0800

    adding dojoSurvey

---
+++ djangoSurvey/submissionForm/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+def result(request):
+    print("Got Request................")
+    print(request.POST)
+    context = {
+        "Name": request.POST['username'],
+        "Dojo_Location": request.POST['dojo_location'],
+        "Favorite_Language": request.POST['favorite_language'],
+        "Comment": request.POST['comment'],
+    }
+    # return redirect("/result")
+    return render(request,"result.html",context)
+
+# def result(request):
+#     return render(request,"result.html")

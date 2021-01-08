96fa328765888c29ba8f8c6ab1f212d334eaa54a
achan <achan@achans-MacBook-Pro.local>
2021-01-08 13:37:59 -0800

    adding single_model_orm

---
+++ single_model_orm/users_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.CharField(max_length=50)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"First Name: {self.first_name}, Last Name {self.last_name}"

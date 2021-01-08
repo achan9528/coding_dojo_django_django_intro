4eef0d9fa03fb7916d19b4bac19350f84397be30
achan <achan@achans-MacBook-Pro.local>
2021-01-08 14:19:39 -0800

    adding template to single_model_orm

--- single_model_orm/users_app/views.py
+++ single_model_orm/users_app/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
 def index(request):
     context = {
-        "welcome_message": "hello!"
+        "welcome_message": "hello!",
+        "users": User.objects.all(),
     }
     return render(request, "index.html", context)
+
+def processForm(request):
+    User.objects.create(
+        first_name = request.POST['firstName'],
+        last_name = request.POST['lastName'],
+        email_address = request.POST['emailAddress'],
+        age = request.POST['age'],
+    )
+    return redirect('/')

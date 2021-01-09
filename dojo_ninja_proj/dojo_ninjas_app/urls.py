b6540f48e01b392e51d0f8d4968f4e2f35f3636b
achan <achan@achans-MacBook-Pro.local>
2021-01-09 15:21:10 -0800

    adding dojo_ninja_proj templates and routes

--- dojo_ninja_proj/dojo_ninjas_app/urls.py
+++ dojo_ninja_proj/dojo_ninjas_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('addDojo', views.addDojo),
+    path('addNinja', views.addNinja),
 ]
--- dojo_ninja_proj/dojo_ninjas_app/views.py
+++ dojo_ninja_proj/dojo_ninjas_app/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from .models import Dojo, Ninja
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello!")
+    context = {
+        "dojos": Dojo.objects.all(),
+        "ninjas": Ninja.objects.all(),
+    }
+    return render(request, "index.html", context)
+
+def addDojo(request):
+    dojoName = request.POST['addDojoName']
+    dojoCity = request.POST['addDojoCity']
+    dojoState = request.POST['addDojoState']
+    Dojo.objects.create(name = dojoName, city= dojoCity, state=dojoState)
+    return redirect('/')
+
+def addNinja(request):
+    ninjaFirstnName = request.POST['addNinjaFirstName']
+    ninjaLastName = request.POST['addNinjaLastName']
+    ninjaDojo = request.POST['selectDojo']
+    Ninja.objects.create(dojo = Dojo.objects.get(name=ninjaDojo), first_name=ninjaFirstnName, last_name=ninjaLastName)
+    return redirect('/')

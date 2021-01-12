b6dc659b10e04ef0e32ef7271acbbf4e2dc747f7
achan <achan@achans-MacBook-Pro.local>
2021-01-11 21:51:05 -0800

    adding books_authors_proj with templates

--- books_authors_proj/books_authors_app/urls.py
+++ books_authors_proj/books_authors_app/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('books',views.index),
+    path('books/<int:bookId>', views.bookInformation),
+    path('authors',views.authors),
+    path('authors/<int:authorId>',views.authorInformation),
+    path('booksAdd',views.booksAdd),
+    path('authorsAdd',views.authorsAdd),
+    path('existingBooksAdd', views.authorsAdd),
+    path('existinAuthorsAdd', views.authorsAdd),
 ]
--- books_authors_proj/books_authors_app/views.py
+++ books_authors_proj/books_authors_app/views.py
@@ -1,5 +1,98 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book, Author
 
 # Create your views here.
 def index(request):
+    labels = zip(["Title", "Description"],["addTitle", "addDescription"])
+    context = {
+        "subject": "books",
+        "formHeader": "Add a Book",
+        "labels": labels,
+        "iterable": Book.objects.all(),
+    }
+    
+    return render(request,"index.html",context)
+
+def authors(request):
+    labels=zip(["First Name", "Last Name", "Notes"], ["addFirstName","addLastName","addNotes"])
+    context = {
+        "subject": "authors",
+        "formHeader": "Add an Author",
+        "labels": labels,
+        "iterable": Author.objects.all(),
+    }
+    
+    return render(request,"index.html",context)
+
+def bookInformation(request, bookId):
+    requestedBook = Book.objects.get(id=bookId)
+    iterable=[]
+    for author in requestedBook.authors.all():
+        name = f"{author.first_name} {author.last_name}"
+        iterable.append(name)
+
+    iterable2 = []
+    for author in Author.objects.all():
+        name = f"{author.first_name} {author.last_name}"
+        if name not in iterable:
+            iterable2.append(name)
+    if len(iterable2) == 0:
+        iterable2.append("No authors left to add!")
+
+    context = {
+        "field0": requestedBook.id,
+        "field1": "Description",
+        "field2": "Authors",
+        "header": requestedBook.title,
+        "iterable": iterable,
+        "options": iterable2,
+        "formAction": "/existingAuthorsAdd",
+    }
+
+    return render(request, "infoDisplay.html", context)
+
+def authorInformation(request, authorId):
+    requestedAuthor = Author.objects.get(id=authorId)
+    iterable=[]
+    for book in requestedAuthor.books.all():
+        name = book.title
+        iterable.append(name)
+
+    iterable2=[]
+    for book in Book.objects.all():
+        name = book.title
+        if name not in iterable:
+            iterable2.append(name)
+    if len(iterable2) == 0:
+        iterable2.append("No books left to add!")
+
+    context = {
+        "field0": requestedAuthor.id,
+        "field1": "Notes",
+        "field2": "Books",
+        "header": f"{requestedAuthor.first_name} {requestedAuthor.last_name}",
+        "iterable": iterable,
+        "options": iterable2,
+        "formAction": "/existingBooksAdd",
+    }
+
+    return render(request, "infoDisplay.html", context)
+
+def booksAdd(request):
+    title = request.POST['addTitle']
+    desc = request.POST['addDescription']
+    Book.objects.create(title=title,desc=desc)
+    return redirect("/")
+
+def authorsAdd(request):
+    first_name = request.POST['addFirstName']
+    last_name = request.POST['addLastName']
+    notes = request.POST['addNotes']
+    Author.objects.create(first_name=first_name,last_name=last_name,notes=notes)
+    return redirect("/")
+
+def existingBooksAdd(request):
+    pass
+
+def existingAuthorsAdd(request):
     pass
